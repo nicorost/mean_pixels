94ba9928d816433322d43f7cb736c5d3aa17b123
nico_rost <nicolas_rost@psych.mpg.de>
2020-03-23 16:31:57 +0100

    removed unneeded code

--- mean_pixels.py
+++ mean_pixels.py
@@ -5,13 +5,11 @@ def calculate_pixel_brightness(x, y, z):
     return (x + y + z) / 3
 
 
-red, green, blue = 100, 0, 0
-pixel_brightness = calculate_pixel_brightness(red, green, blue)
+pixel = {'red': 100, 'green': 0, 'blue': 0}
+pixel_brightness = calculate_pixel_brightness(pixel['red'], pixel['green'], pixel['blue'])
 
-red2, green2, blue2 = 50, 100, 0
-pixel_brightness2 = calculate_pixel_brightness(red2, green2, blue2)
-
-readiness, readiness = 0, 200
+pixel2 = {'red': 50, 'green': 100, 'blue': 0}
+pixel_brightness2 = calculate_pixel_brightness(pixel2['red'], pixel2['green'], pixel2['blue'])
 
 pixel_brightness3 = (pixel_brightness + pixel_brightness2) / 2
 print(pixel_brightness3)
