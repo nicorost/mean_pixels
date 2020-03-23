446bd9cc5305af250addbd6b834bdf5884f07d62
nico_rost <nicolas_rost@psych.mpg.de>
2020-03-23 16:11:24 +0100

    shortened code

--- mean_pixels.py
+++ mean_pixels.py
@@ -1,17 +1,12 @@
 # This is my project!
 
-red = 100
-green = 0
-blue = 0
+red, green, blue = 100, 0, 0
 pixel_brightness = (red + green + blue) / 3
 
-red2 = 50
-green2 = 100
-blue2 = 0
+red2, green2, blue2 = 50, 100, 0
 pixel_brightness2 = (red2 + green2 + blue2) / 3
 
-readiness = 0
-readiness2 = 200
+readiness, readiness = 0, 200
 
 pixel_brightness3 = (pixel_brightness + pixel_brightness2) / 2
 print(pixel_brightness3)
