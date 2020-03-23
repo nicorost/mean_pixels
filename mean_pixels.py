b09efbe09a265599038b63462479578af6efabbd
nico_rost <nicolas_rost@psych.mpg.de>
2020-03-23 15:50:14 +0100

    more speaking variable names

--- mean_pixels.py
+++ mean_pixels.py
@@ -12,10 +12,8 @@ BlueValue2 = 0
 ReadinessValue = 0
 ReadinessValue2 = 200
 
-# Get pixel brightnesses
-result = (RedValue + GreenValue + BlueValue) / 3
-result2 = (RedValue2 + GreenValue2 + BlueValue2) / 3
+pixel_brightness = (RedValue + GreenValue + BlueValue) / 3
+pixel_brightness2 = (RedValue2 + GreenValue2 + BlueValue2) / 3
 
-# Get mean brightness
-result3 = (result + result2) / 2
-print(result3)
+pixel_brightness3 = (pixel_brightness + pixel_brightness2) / 2
+print(pixel_brightness3)
